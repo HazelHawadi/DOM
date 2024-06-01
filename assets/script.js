53fde6df58e4b1a465c2829a0b5885af74348f01
Hazel Hawadi <hawadihazel@icloud.com>
2024-06-01 13:14:43 +0000

    add questions layout sections

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: '342 + 46',
+        answers: [
+            { text: '543', correct: true },
+            { text: '366', correct: false },
+            { text: '388', correct: false },
+            { text: '368', correct: false }
+        ]
+    },
+];
