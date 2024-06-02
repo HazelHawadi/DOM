a83705440179801a26af9362b92419be8b4df93e
Hazel Hawadi <hawadihazel@icloud.com>
2024-06-02 09:22:44 +0000

    change the font family for the whole quize page

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,138 @@
 const questions = [
     {
-        question: '342 + 46',
+        question: '',
         answers: [
-            { text: '543', correct: true },
-            { text: '366', correct: false },
-            { text: '388', correct: false },
-            { text: '368', correct: false }
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: true },
+            { choiceD: '', correct: false }
         ]
     },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: true },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: true }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: true },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: true },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: true }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: 'Earth', correct: false },
+            { choiceB: 'Mars', correct: true },
+            { choiceC: 'Jupiter', correct: false },
+            { choiceD: 'Venus', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: true },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: true }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: true },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: true },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: true },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: true },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: true },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: false },
+            { choiceC: '', correct: true },
+            { choiceD: '', correct: false }
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            { choiceA: '', correct: false },
+            { choiceB: '', correct: true },
+            { choiceC: '', correct: false },
+            { choiceD: '', correct: true }
+        ]
+    },
+    
 ];
