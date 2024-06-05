ca646df2b379c30d575417096c2e253cbe2862c4
Hazel Hawadi <hawadihazel@icloud.com>
2024-06-05 00:22:44 +0000

    add two button styles for the answer buttons to highlight green when answer is correct and red when its wrong

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,131 +1,128 @@
 const questions = [
     {
-        //JavaScript questions for the quiz
         question: 'What is the correct syntax to create a new object in JavaScript?',
         answers: [
-            { choiceA: 'var obj = [];', correct: false },
-            { choiceB: 'var obj = new Array();', correct: false },
-            { choiceC: 'var obj = {};', correct: true },
-            { choiceD: 'var obj = Object[];', correct: false }
+            { text: 'var obj = [];', correct: false },
+            { text: 'var obj = new Array();', correct: false },
+            { text: 'var obj = {};', correct: true },
+            { text: 'var obj = Object[];', correct: false }
         ]
     },
     {
         question: 'Which method can be used to find the length of a string?',
         answers: [
-            { choiceA: 'length()', correct: false },
-            { choiceB: 'length', correct: true },
-            { choiceC: 'len()', correct: false },
-            { choiceD: 'size()', correct: false }
+            { text: 'length()', correct: false },
+            { text: 'length', correct: true },
+            { text: 'len()', correct: false },
+            { text: 'size()', correct: false }
         ]
     },
     {
         question: 'How do you write an arrow function in JavaScript?',
         answers: [
-            { choiceA: 'function myFunction() => {}', correct: false },
-            { choiceB: 'myFunction() => {}', correct: false },
-            { choiceC: 'var myFunction = function() {}', correct: false },
-            { choiceD: 'const myFunction = () => {}', correct: true }
+            { text: 'function myFunction() => {}', correct: false },
+            { text: 'myFunction() => {}', correct: false },
+            { text: 'var myFunction = function() {}', correct: false },
+            { text: 'const myFunction = () => {}', correct: true }
         ]
     },
     {
-        question: 'What is the output of the following code: "console.log(typeof null);"?'
+        question: 'What is the output of the following code: "console.log(typeof null);"?',
         answers: [
-            { choiceA: 'null', correct: false },
-            { choiceB: 'object', correct: true },
-            { choiceC: 'undefined', correct: false },
-            { choiceD: 'number', correct: false }
+            { text: 'null', correct: false },
+            { text: 'object', correct: true },
+            { text: 'undefined', correct: false },
+            { text: 'number', correct: false }
         ]
     },
     {
         question: 'Which of the following is a correct way to create an array in JavaScript?',
         answers: [
-            { choiceA: 'var arr = array(1, 2, 3);', correct: false },
-            { choiceB: 'var arr = [1, 2, 3];', correct: true },
-            { choiceC: 'var arr = (1, 2, 3);', correct: false },
-            { choiceD: 'Vevar arr = {1, 2, 3};nus', correct: false }
+            { text: 'var arr = array(1, 2, 3);', correct: false },
+            { text: 'var arr = [1, 2, 3];', correct: true },
+            { text: 'var arr = (1, 2, 3);', correct: false },
+            { text: 'var arr = {1, 2, 3};', correct: false }
         ]
     },
     {
-        //HTML questions for the quiz
         question: 'What does HTML stand for?',
         answers: [
-            { choiceA: 'Hyper Text Markup Language', correct: true },
-            { choiceB: 'Hyper Trainer Marking Language', correct: false },
-            { choiceC: 'Hyper Text Marketing Language', correct: false },
-            { choiceD: 'Hyper Text Markup Leveler', correct: false }
+            { text: 'Hyper Text Markup Language', correct: true },
+            { text: 'Hyper Trainer Marking Language', correct: false },
+            { text: 'Hyper Text Marketing Language', correct: false },
+            { text: 'Hyper Text Markup Leveler', correct: false }
         ]
     },
     {
         question: 'Which HTML tag is used to define an internal style sheet?',
         answers: [
-            { choiceA: '<script>', correct: false },
-            { choiceB: '<css>', correct: false },
-            { choiceC: '<style>', correct: true },
-            { choiceD: '<link>', correct: false }
+            { text: '<script>', correct: false },
+            { text: '<css>', correct: false },
+            { text: '<style>', correct: true },
+            { text: '<link>', correct: false }
         ]
     },
     {
         question: 'How can you create a hyperlink in HTML?',
         answers: [
-            { choiceA: '<a link="url">link text</a>', correct: false },
-            { choiceB: '<link href="url">link text</link>', correct: false },
-            { choiceC: '<a url="url">link text</a>', correct: false },
-            { choiceD: '<a href="url">link text</a>', correct: true }
+            { text: '<a link="url">link text</a>', correct: false },
+            { text: '<link href="url">link text</link>', correct: false },
+            { text: '<a url="url">link text</a>', correct: false },
+            { text: '<a href="url">link text</a>', correct: true }
         ]
     },
     {
-        question: ' Which HTML element is used to specify a footer for a document or section?',
+        question: 'Which HTML element is used to specify a footer for a document or section?',
         answers: [
-            { choiceA: '<footer>', correct: true },
-            { choiceB: '<bottom>', correct: false },
-            { choiceC: '<section>', correct: false },
-            { choiceD: '<foot>', correct: false }
+            { text: '<footer>', correct: true },
+            { text: '<bottom>', correct: false },
+            { text: '<section>', correct: false },
+            { text: '<foot>', correct: false }
         ]
     },
     {
         question: 'What is the purpose of the "<head>" element in HTML?',
         answers: [
-            { choiceA: 'To include the main content of the document', correct: false },
-            { choiceB: 'To create a header for the document', correct: false },
-            { choiceC: 'To contain metadata about the document', correct: true },
-            { choiceD: 'To specify the footer of the document', correct: false }
+            { text: 'To include the main content of the document', correct: false },
+            { text: 'To create a header for the document', correct: false },
+            { text: 'To contain metadata about the document', correct: true },
+            { text: 'To specify the footer of the document', correct: false }
         ]
     },
     {
-        //CSS questions for the quiz
         question: 'What does CSS stand for?',
         answers: [
-            { choiceA: 'Cascading Style Sheets', correct: true },
-            { choiceB: 'Colorful Style Sheets', correct: false },
-            { choiceC: 'Creative Style Sheets', correct: false },
-            { choiceD: 'Computer Style Sheets', correct: false }
+            { text: 'Cascading Style Sheets', correct: true },
+            { text: 'Colorful Style Sheets', correct: false },
+            { text: 'Creative Style Sheets', correct: false },
+            { text: 'Computer Style Sheets', correct: false }
         ]
     },
     {
         question: 'How do you add a background color in CSS?',
         answers: [
-            { choiceA: 'color: red;', correct: false },
-            { choiceB: 'background-color: red;', correct: true },
-            { choiceC: 'bg-color: red;', correct: false },
-            { choiceD: 'background: red;', correct: false }
+            { text: 'color: red;', correct: false },
+            { text: 'background-color: red;', correct: true },
+            { text: 'bg-color: red;', correct: false },
+            { text: 'background: red;', correct: false }
         ]
     },
     {
         question: 'Which property is used to change the text color of an element?',
         answers: [
-            { choiceA: 'text-color', correct: false },
-            { choiceB: 'fgcolor', correct: false },
-            { choiceC: 'color', correct: true },
-            { choiceD: 'font-color', correct: false }
+            { text: 'text-color', correct: false },
+            { text: 'fgcolor', correct: false },
+            { text: 'color', correct: true },
+            { text: 'font-color', correct: false }
         ]
     },
     {
         question: 'How do you select an element with id "demo" in CSS?',
         answers: [
-            { choiceA: '.demo', correct: false },
-            { choiceB: '*demo', correct: false },
-            { choiceC: '#demo', correct: true },
-            { choiceD: 'demo', correct: false }
+            { text: '.demo', correct: false },
+            { text: '*demo', correct: false },
+            { text: '#demo', correct: true },
+            { text: 'demo', correct: false }
         ]
-    },
+    }
 ];
